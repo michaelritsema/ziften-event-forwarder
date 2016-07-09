f4c190891578283e91d0b81bb286deef98bdcb62
Michael Ritsema <michael.ritsema@ziften.com>
2016-07-09 12:32:12 -0500

    added protobuf msg sources gitconfig

---
+++ proto/import.go
@@ -0,0 +1,12 @@
+package main
+
+/*
+	Used to bring down Ziften protobuf message sources
+	Sources will be in
+	$GO_HOME/src/github.com/ziften/protocolbuffers/Protobuf/
+	or
+	../../ziften/protocolbuffer/Protobuf/
+*/
+import (
+	"github.com/ziften/protocolbuffers"
+)
